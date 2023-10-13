674f3d2af20abee9e6456124dd9bde2fba28c664
Sakif Hridoy <sakifworkspace@gmail.com>
2023-10-14 01:55:43 +0600

    add toggle loafing spinner

--- js/phone.js
+++ js/phone.js
@@ -65,11 +65,13 @@ else{
     //step 4
     phoneContainer.appendChild(phoneCard);
   });
+  // hide toggleSpinner
+  toggleLoadingSpinner(false)
 };
 
 const handleSearch = () => {
   // console.log('search clicked')
-
+  toggleLoadingSpinner(true)
   const searchField = document.getElementById("search-field");
   const searchText = searchField.value;
   // console.log(searchText)
@@ -78,9 +80,21 @@ const handleSearch = () => {
 
 const handleSearch2 = () => {
   // console.log('search clicked')
-
+  toggleLoadingSpinner(true)
   const searchField = document.getElementById("search-field2");
   const searchText = searchField.value;
   // console.log(searchText)
   loadPhone(searchText);
 };
+
+
+const toggleLoadingSpinner = (isLoading)=>{
+  const loadingSpinner = document.getElementById('loading-spinner');
+  if(isLoading){
+    loadingSpinner.classList.remove('hidden');
+  }
+  else{
+    loadingSpinner.classList.add('hidden');
+
+  }
+}
